e11a78d70ba2bbbd7501eb0ca584138d679c2ae5
tongliu1234 <tl3@andrew.cmu.edu>
2023-12-07 16:00:55 -0500

    Added dummy data for posts and comments

---
+++ dummy_data.py
@@ -0,0 +1,43 @@
+from Reddit_pb2 import Comment, Post
+
+# Dummy in-memory storage
+posts = {
+    "1": Post(
+        title="First post(Default)",
+        text="This is the first post",
+        author="user1",
+        video_url="#",
+        score=0,
+        post_state=Post.NORMAL,
+        post_id="1",
+    ),
+}
+comments = {
+    "1": Comment(
+        text="This is the first comment",
+        author="user1",
+        score=0,
+        comment_state=Comment.NORMAL,
+        comment_id="1",
+        parent_id="1",
+        under_post=True,
+    ),
+    "2": Comment(
+        text="This is the second comment",
+        author="user2",
+        score=5,
+        comment_state=Comment.NORMAL,
+        comment_id="2",
+        parent_id="1",
+        under_post=True,
+    ),
+    "3": Comment(
+        text="This is the third comment",
+        author="user3",
+        score=10,
+        comment_state=Comment.NORMAL,
+        comment_id="3",
+        parent_id="1",
+        under_post=True,
+    ),
+}
